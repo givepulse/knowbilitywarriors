259288b9d374fe6e55b338a677e118d6812481ae
George Luc <george@givepulse.com>
2013-12-02 12:48:10 -0600

    cleaning up structure for header before working on style

---
+++ layouts/header.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" href="../css/style.css">
+<script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="../js/jquery-accessibleMegaMenu.js"></script>
+<script type="text/javascript" src="../js/script.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="../js/html5shiv.js"></script>
+<![endif]-->
+</head>
+<body>
+
+<?php include '../templates/nav.php'; ?>
+
+<header>
+	<h1>
+		<img src="../images/aha_logo.png" alt="Accessible Housing Austin! AHA!">
+	</h1>
+</header>
--- news/index.php
+++ news/index.php
@@ -3,23 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>AHA! in the News</title>
-<link rel="stylesheet" href="../css/style.css">
-<script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
-<script type="text/javascript" src="../js/jquery-accessibleMegaMenu.js"></script>
-<script type="text/javascript" src="../js/script.js"></script>
-<!--[if lt IE 9]>
-<script type="text/javascript" src="../js/html5shiv.js"></script>
-<![endif]-->
-</head>
-<body>
-
-<?php include '../templates/nav.php'; ?>
-
-<header>
-	<h1>
-		<img src="../images/aha_logo.png" alt="Accessible Housing Austin! AHA!">
-	</h1>
-</header>
+<?php include '../layouts/header.php'; ?>
 
 <main id="mainContent" role="main" tabindex="-1">
 	<h2><abbr title="Accessible Housing Austin">AHA</abbr>! in the News</h2>
