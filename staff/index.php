ef0e2849450e3c069fda3da0aed642c9a0d33bb4
George Luc <george@givepulse.com>
2013-12-04 22:03:54 -0600

    updates to staff and story pages

--- staff/index.php
+++ staff/index.php
@@ -6,55 +6,93 @@
 <?php include '../layouts/header.php'; ?>
 
 <main id="mainContent" role="main" tabindex="-1">
-	<h2>Board &amp; Staff</h2>
-
-	<h3>Isabelle Headrick, Executive Director</h3>
-	<img src="../images/isabelleheadrick.jpg" alt="">
-	<p>
-		Isabelle Headrick has been involved in affordable housing since she first volunteered in a homeless women’s shelter as a teenager in Chicago. Before joining AHA! in 2012, she served for ten years as the Executive Director of Blackland Community Development Corporation, developing, managing and advocating for housing for people with disabilities, seniors, and homeless families. Isabelle has lived in Austin for twenty years and holds a B.A. from Oberlin College and an M.A. from the University of Texas at Austin. She has served as Co-Chair of the Austin Community Housing Development Organization Roundtable and participates on the Public Policy Committee of One Voice Central Texas. Outside of work, she swims, camps and feeds her children, which, as the mother of two teenage boys, is no small accomplishment. She loves the fact that AHA! is about more than housing: along with security, it helps people achieve independence, comfort, and dignity and allows them to participate in the life of their Austin neighborhoods.
-	</p>
-
-	<h3>Stephanie Thomas, President</h3>
-	<img src="../images/stephaniethomas.jpg" alt="">
-	<p>
-		Stephanie Thomas has worked in disability rights and housing for nearly four decades.   As a person in a wheelchair, she knows firsthand the difference that accessible housing makes in improving one’s quality of life. Stephanie led the effort for the 1996 voluntary compliance agreement between ADAPT and the City of Austin to address widespread housing discrimination against people with disabilities: this led to Austin’s Visitability Ordinance, the accessibility in SMART housing, and the development of hundreds more accessible units in Austin. Prior to Austin, Stephanie worked with the Atlantis Community in Denver where she developed positive integrated housing options. In addition, she has authored numerous articles, white papers and other informational pieces on housing rights for people with disabilities.  
-	</p>
-	<p>
-		As one of the founders of AHA!, Stephanie is proud of how the organization operates, partnering people with disabilities with people of various housing expertise to create viable integrated, accessible housing solutions that are affordable to low-income families and individuals.
-	</p>
-
-	<h3>Jennifer, McPhail, Vice President</h3>
-	<img src="../images/jennifermcphail.jpg" alt="">
-	<p>
-		A life-long Austinite, Jennifer McPhail has served as a national and local community organizer for ADAPT for 23 years, working to improve long term services and  supports so that people with disabilities who need services can live in the community. She has also worked to improve sidewalk access throughout Austin, increase access to local businesses and increase the availability of affordable, accessible, integrated housing.  She has twice been appointed to the City of Austin’s Bond Advisory Committee and has volunteered with the Community Emergency Response Team. She served on the Texas Democratic Party Chairman’s Platform Advisory Committee in 2004, 2006,2008,  and 2010  and represented Senate District 14 at the 2004 National Convention. She joined the AHA! Board of Directors because she believes that the lack of affordable, accessible, integrated  housing  is one of the biggest problems the disability community faces now and in the future and  looks forward to more success for AHA! and its tenants. 
-	</p>
-
-	<h3>Paul Hilgers, Secretary</h3>
-	<img src="../images/paulhilgers.jpg" alt="">
-	<p>
-		Paul Hilgers is CEO of the Austin Board of REALTORS®, a nearly 10,000-member professional organization that serves 18 Central Texas counties. Prior to his current position, Hilgers served nearly 10 years as the City of Austin’s Director of Neighborhood Housing and Community Development managing federal and local housing and community development programs in Austin, including the expansion of the Austin Housing Finance Corporation, which issues housing bonds and tax credits.  More recently he served three years as deputy general manager of the reorganized Pedernales Electric Cooperative (PEC) to help bring major changes to that organization. 
-	</p>
-	<p>
-		An Austin native, Hilgers is a graduate of Whittier College and holds a Masters degree from the Lyndon B. Johnson School of Public Affairs at The University of Texas at Austin.   In addition to the City of Austin and the PEC, he has worked for former Governor Mark White, as a long-time district director for former Congressman Jake Pickle, and as a federal and community relations manager for the Lower Colorado River Authority.
-	</p>
-
-	<h3>Jean Langendorf, Treasurer</h3>
-	<img src="../images/jeanlangendorf.jpg" alt="">
-	<p>
-		Jean Langendorf is the Vice President of Community and Housing Services at Easter Seals Central Texas. Prior to her current position, Jean was the Executive Director of United Cerebral Palsy of Texas (UCP Texas), which merged with Easter Seals in the beginning of 2010. During her tenure with UCP, she also served as the Project Director for the Texas Home of Your Own Coalition, a  project designed to expand homeownership opportunities to Texans with disabilities. Jean has served as a consultant providing technical assistance to Public Housing Authorities for Project Access and Fannie Mae, as well as to housing and homeownership programs throughout the United States. Jean was one of the disability advocates who helped to found Accessible Housing Austin! to address the great need for accessible and affordable housing in Austin.  She is thrilled to now be a member of the board and to see the growth the organization has experienced!
-	</p>
-
-	<h3>Dana Carpenter, Member</h3>
-	<img src="../images/danacarpenter.jpg" alt="">
-
-	<h3>Becca Bruce Dobberfuhl, Member</h3>
-	<img src="../images/beccadobberfuhl.jpg" alt="">
-	<p>
-		Becca Bruce Dobberfuhl  has more than 15 years of experience in the areas of affordable housing and community development. As president and founder of Vacri Development, Becca consults with both nonprofit and for-profit organizations interested in developing or expanding affordable housing opportunities. Becca has worked for a wide range of clients from small, community-based nonprofits to large, national nonprofits to develop affordable housing and secure financing. Prior to starting her own firm, Becca oversaw the development of affordable housing projects and managed the acquisition of financing through a variety of federal, state, and local programs in her position as Vice President of Diana McIver &amp; Associates and Development Manager for Foundation Communities, Inc. Becca has secured more than $100 million for affordable housing development and served as development manager for more than 30 multifamily projects across the country. 
-	</p>
-	<p>
-		Becca holds a Masters Degree in Community and Regional Planning from the University of Texas at Austin. She was a member of the 2003-04 Leadership Austin class and served on the board of GENAustin (Girls Empowerment Network) from 2002- 2006. She is a mother of a toddler, and this job currently takes up most of her time.
-	</p>
+	
+    <h2>Board &amp; Staff</h2>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/stephaniethomas.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Stephanie Thomas, President</h3>
+            <p>
+                Stephanie Thomas has worked in disability rights and housing for nearly four decades.   As a person in a wheelchair, she knows firsthand the difference that accessible housing makes in improving one’s quality of life. Stephanie led the effort for the 1996 voluntary compliance agreement between ADAPT and the City of Austin to address widespread housing discrimination against people with disabilities: this led to Austin’s Visitability Ordinance, the accessibility in SMART housing, and the development of hundreds more accessible units in Austin. Prior to Austin, Stephanie worked with the Atlantis Community in Denver where she developed positive integrated housing options. In addition, she has authored numerous articles, white papers and other informational pieces on housing rights for people with disabilities.  
+            </p>
+            <p>
+                As one of the founders of AHA!, Stephanie is proud of how the organization operates, partnering people with disabilities with people of various housing expertise to create viable integrated, accessible housing solutions that are affordable to low-income families and individuals.
+            </p>
+        </div>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/jennifermcphail.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Jennifer, McPhail, Vice President</h3>
+            <p>
+                A life-long Austinite, Jennifer McPhail has served as a national and local community organizer for ADAPT for 23 years, working to improve long term services and  supports so that people with disabilities who need services can live in the community. She has also worked to improve sidewalk access throughout Austin, increase access to local businesses and increase the availability of affordable, accessible, integrated housing.  She has twice been appointed to the City of Austin’s Bond Advisory Committee and has volunteered with the Community Emergency Response Team. She served on the Texas Democratic Party Chairman’s Platform Advisory Committee in 2004, 2006,2008,  and 2010  and represented Senate District 14 at the 2004 National Convention. She joined the AHA! Board of Directors because she believes that the lack of affordable, accessible, integrated  housing  is one of the biggest problems the disability community faces now and in the future and  looks forward to more success for AHA! and its tenants. 
+            </p>
+        </div>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/paulhilgers.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Paul Hilgers, Secretary</h3>
+            <p>
+                Paul Hilgers is CEO of the Austin Board of REALTORS®, a nearly 10,000-member professional organization that serves 18 Central Texas counties. Prior to his current position, Hilgers served nearly 10 years as the City of Austin’s Director of Neighborhood Housing and Community Development managing federal and local housing and community development programs in Austin, including the expansion of the Austin Housing Finance Corporation, which issues housing bonds and tax credits.  More recently he served three years as deputy general manager of the reorganized Pedernales Electric Cooperative (PEC) to help bring major changes to that organization. 
+            </p>
+            <p>
+                An Austin native, Hilgers is a graduate of Whittier College and holds a Masters degree from the Lyndon B. Johnson School of Public Affairs at The University of Texas at Austin.   In addition to the City of Austin and the PEC, he has worked for former Governor Mark White, as a long-time district director for former Congressman Jake Pickle, and as a federal and community relations manager for the Lower Colorado River Authority.
+            </p>
+        </div>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/jeanlangendorf.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Jean Langendorf, Treasurer</h3>
+            <p>
+                Jean Langendorf is the Vice President of Community and Housing Services at Easter Seals Central Texas. Prior to her current position, Jean was the Executive Director of United Cerebral Palsy of Texas (UCP Texas), which merged with Easter Seals in the beginning of 2010. During her tenure with UCP, she also served as the Project Director for the Texas Home of Your Own Coalition, a  project designed to expand home-ownership opportunities to Texans with disabilities. Jean has served as a consultant providing technical assistance to Public Housing Authorities for Project Access and Fannie Mae, as well as to housing and homeownership programs throughout the United States. Jean was one of the disability advocates who helped to found Accessible Housing Austin! to address the great need for accessible and affordable housing in Austin.  She is thrilled to now be a member of the board and to see the growth the organization has experienced!
+            </p>
+        </div>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/danacarpenter.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Dana Carpenter, Member</h3>
+            <p>
+            </p>
+        </div>
+    </div>
+    <div class="clearfix" style="margin-top:1em;">
+        <div class="col-md-2">
+                <img src="../images/beccadobberfuhl.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Becca Bruce Dobberfuhl, Member</h3>
+            <p>
+                Becca Bruce Dobberfuhl  has more than 15 years of experience in the areas of affordable housing and community development. As president and founder of Vacri Development, Becca consults with both nonprofit and for-profit organizations interested in developing or expanding affordable housing opportunities. Becca has worked for a wide range of clients from small, community-based nonprofits to large, national nonprofits to develop affordable housing and secure financing. Prior to starting her own firm, Becca oversaw the development of affordable housing projects and managed the acquisition of financing through a variety of federal, state, and local programs in her position as Vice President of Diana McIver &amp; Associates and Development Manager for Foundation Communities, Inc. Becca has secured more than $100 million for affordable housing development and served as development manager for more than 30 multifamily projects across the country. 
+            </p>
+            <p>
+                Becca holds a Masters Degree in Community and Regional Planning from the University of Texas at Austin. She was a member of the 2003-04 Leadership Austin class and served on the board of GENAustin (Girls Empowerment Network) from 2002- 2006. She is a mother of a toddler, and this job currently takes up most of her time.
+            </p>
+        </div>
+    </div>
+    <div class="clearfix">
+        <div class="col-md-2">
+            <img src="../images/isabelleheadrick.jpg" width="150px" class="img-rounded" alt="">
+        </div>
+        <div class="col-md-10">
+            <h3 style="margin-top:0;">Isabelle Headrick, Executive Director</h3>
+            <p>
+                Isabelle Headrick has been involved in affordable housing since she first volunteered in a homeless women’s shelter as a teenager in Chicago. Before joining AHA! in 2012, she served for ten years as the Executive Director of Blackland Community Development Corporation, developing, managing and advocating for housing for people with disabilities, seniors, and homeless families. Isabelle has lived in Austin for twenty years and holds a B.A. from Oberlin College and an M.A. from the University of Texas at Austin. She has served as Co-Chair of the Austin Community Housing Development Organization Roundtable and participates on the Public Policy Committee of One Voice Central Texas. Outside of work, she swims, camps and feeds her children, which, as the mother of two teenage boys, is no small accomplishment. She loves the fact that AHA! is about more than housing: along with security, it helps people achieve independence, comfort, and dignity and allows them to participate in the life of their Austin neighborhoods.
+            </p>
+        </div>
+    </div>
 </main>
 
 <?php include '../templates/footer.php'; ?>
--- story/index.php
+++ story/index.php
@@ -6,47 +6,57 @@
 <?php include '../layouts/header.php'; ?>
 
 <main id="mainContent" role="main" tabindex="-1">
-	<h2>The Story of AHA!</h2>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <h2>The Story of AHA!</h2>
+        </div>
+    </div>
+    <div class="col-md-4" style="">
+        <img src="../images/fairhousinglogo.png" width="280px" alt="Equal Housing Opportunity">
 
-	<img src="../images/fairhousinglogo.png" alt="Equal Housing Opportunity">
+        <img src="../images/elainna.jpg" width="280px" style="margin-top:2em;" alt="Young woman smiling">
+    </div>
+    <div class="col-md-8">
+        <div style="margin: 2em 0;">
+            <aside role="complementary">
+                <p style="margin-bottom:1em;"><span class="label label-default" style="font-size:130%;">$700</span>
+                The average Social Security received by a person with a disability each month</p>
+                <p><span class="label label-default" style="font-size:130%;">$1,100</span>
+                The average monthly rent for a two-bedroom apartment in Austin</p>
+                <p style="font-size:90%; font-style:italic;">Source: <a href="http://average-rent.findthedata.org/">FindtheData</a>. Average Apartment Rent,
+                Refine Search terms: “787” (zip code), travis (county), Texas (state)</p>
+            </aside>
+        </div>
+        <p>
+            With a <span class="label label-warning" style="font-size:110%;">73% unemployment rate</span>, people with disabilities are overrepresented in Austin’s
+            low-income population. The fair market rent for a 2-bedroom apartment in Austin is
+            <span class="label label-warning" style="font-size:110%;">$1,050</span>; yet the average person with a disability receives less than <span class="label label-warning" style="font-size:110%;">$700 per month</span> in
+            Social Security. This has resulted in a <span class="label label-danger" style="font-size:110%;">shortage of 6,000 homes</span> for low-income households
+            that include a person with disabilities. </p>
+        <p>
+            <div class="alert alert-danger" style="font-size:110%;">
+                As housing prices rise, it is increasingly
+            difficult to find homes that are affordable, accessible, and integrated into safe,
+            transit-oriented, and amenity-rich neighborhoods.
+            </div>
+        </p>
 
-	<img src="../images/elainna.jpg" alt="Young woman smiling">
-
-	<aside role="complementary">
-		<p>$700
-		The average Social Security received by a person with a disability each month</p>
-		<p>$1,100
-		The average monthly rent for a two-bedroom apartment in Austin</p>
-		<p>Source: <a href="http://average-rent.findthedata.org/">FindtheData</a>. Average Apartment Rent,
-		Refine Search terms: “787” (zip code), travis (county), Texas (state)</p>
-	</aside>
-
-	<p>
-		With a 73% unemployment rate, people with disabilities are overrepresented in Austin’s
-		low-income population. The fair market rent for a 2-bedroom apartment in Austin is
-		$1,050; yet the average person with a disability receives less than $700 per month in
-		Social Security. This has resulted in a shortage of 6,000 homes for low-income households
-		that include a person with disabilities. As housing prices rise, it is increasingly
-		difficult to find homes that are affordable, accessible, and integrated into safe,
-		transit-oriented, and amenity-rich neighborhoods.
-	</p>
-
-	<p>
-		Accessible Housing Austin! was founded in 2005 by disability rights advocates who
-		recognized that the scarcity of housing was a critical issue for people with disabilities.
-		AHA! is the only housing non-profit in Austin led by members of the disability community
-		whose mission is to provide affordable, accessible, integrated housing for low-income
-		people with disabilities.
-	</p>
-
-	<p>
-		We serve a diverse range of Austinites with disabilities, including adults and children
-		with mobility impairments, visual impairment, chronic illness, and other physical and
-		emotional disabilities.
-	</p>
+        <p class="alert alert-success">
+            Accessible Housing Austin! was founded in 2005 by disability rights advocates who
+            recognized that the scarcity of housing was a critical issue for people with disabilities.
+            AHA! is the only housing non-profit in Austin led by members of the disability community
+            whose mission is to provide affordable, accessible, integrated housing for low-income
+            people with disabilities.
+        </p>
+        <p class="alert alert-success">
+            We serve a diverse range of Austinites with disabilities, including adults and children
+            with mobility impairments, visual impairment, chronic illness, and other physical and
+            emotional disabilities.
+        </p>
+    </div>
 </main>
-
 <?php include '../templates/footer.php'; ?>
+
 
 </body>
 </html>
