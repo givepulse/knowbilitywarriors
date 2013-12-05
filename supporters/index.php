4ea570ed24b07434aa7925bda973169927b68dc8
George Luc <george@givepulse.com>
2013-12-04 22:36:33 -0600

    styling supporters page

--- supporters/index.php
+++ supporters/index.php
@@ -6,26 +6,28 @@
 <?php include '../layouts/header.php'; ?>
 
 <main id="mainContent" role="main" tabindex="-1">
-	<h2>Supporters and Partners</h2>
-
-	<img src="../images/volunteers.jpg" alt="Volunteers">
-	<img src="../images/ilivehereigivehere.png" alt="I live here. I give here.">
-	<img src="../images/frost.png" alt="Frost Bank">
-
-	<ul>
-		<li><a href="http://www.adapt.org/">ADAPT of Texas</a>
-		<li><a href="http://www.amerigroup.com/">Amerigroup</a>
-		<li><a href="http://www.caroldochenrealtors.com/">Carol Dochen Realtors</a>
-		<li><a href="http://austintexas.gov/department/housing">City of Austin Neighborhood Housing and Community Development</a>
-		<li><a href="http://demayoassociates.com/">De Mayo and Associates</a>
-		<li><a href="https://www.fhlb.com/">Federal Home Loan Bank of Dallas</a>
-		<li><a href="https://www.frostbank.com/Pages/default.aspx">Frost Bank</a>
-		<li><a href="http://hacanet.org/">Housing Authority of the City of Austin</a>
-		<li><a href="http://www.knowbility.org/">Knowbility</a>
-		<li><a href="http://www.tdhca.state.tx.us/">Texas Department of Housing and Community Affairs</a>
-		<li><a href="http://www.tsahc.org/donors-investors/texas-foundations-fund">Texas Foundations Fund</a>
-		<li><a href="http://www.tsahc.org/">Texas State Affordable Housing Corporation</a>
-	</ul>
+    <h2>Supporters and Partners</h2>
+    <div class="clearfix col-md-5" style="padding-left:0px;">
+        <img src="../images/volunteers.jpg" class="img-rounded" width="400px" alt="Volunteers">
+    </div>
+    <div class="clearfix col-md-7" style="vertical-align:middle;">
+        <img src="../images/ilivehereigivehere.png" width="200px" alt="I live here. I give here.">
+        <img src="../images/frost.png" width="200px" alt="Frost Bank">
+        <ul style="margin-top:1em;">
+            <li><a href="http://www.adapt.org/">ADAPT of Texas</a>
+            <li><a href="http://www.amerigroup.com/">Amerigroup</a>
+            <li><a href="http://www.caroldochenrealtors.com/">Carol Dochen Realtors</a>
+            <li><a href="http://austintexas.gov/department/housing">City of Austin Neighborhood Housing and Community Development</a>
+            <li><a href="http://demayoassociates.com/">De Mayo and Associates</a>
+            <li><a href="https://www.fhlb.com/">Federal Home Loan Bank of Dallas</a>
+            <li><a href="https://www.frostbank.com/Pages/default.aspx">Frost Bank</a>
+            <li><a href="http://hacanet.org/">Housing Authority of the City of Austin</a>
+            <li><a href="http://www.knowbility.org/">Knowbility</a>
+            <li><a href="http://www.tdhca.state.tx.us/">Texas Department of Housing and Community Affairs</a>
+            <li><a href="http://www.tsahc.org/donors-investors/texas-foundations-fund">Texas Foundations Fund</a>
+            <li><a href="http://www.tsahc.org/">Texas State Affordable Housing Corporation</a>
+        </ul>
+    </div>
 </main>
 
 <?php include '../templates/footer.php'; ?>
