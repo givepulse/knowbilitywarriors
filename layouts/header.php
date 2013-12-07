c2d5b84cbf2a7590d146960bc8f27a2d23dc81d9
Shawn Lauriat <slauriat@gmail.com>
2013-12-06 19:12:47 -0500

    Fixing some issues noted by Isabelle.

--- layouts/header.php
+++ layouts/header.php
@@ -1,8 +1,8 @@
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
 <link rel="stylesheet" href="../css/style.css">
 <script type="text/javascript" src="../js/jquery-1.10.2.min.js"></script>
 <script type="text/javascript" src="../js/jquery-accessibleMegaMenu.js"></script>
 <script type="text/javascript" src="../js/script.js"></script>
-<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
 <!--[if lt IE 9]>
 <script type="text/javascript" src="../js/html5shiv.js"></script>
 <![endif]-->
@@ -11,7 +11,9 @@
 <div class="row">
     <div class="col-md-2">
         <header style="margin: 1em 0;">
+		<a href="../">
 			<img id="aha-logo" src="../images/aha_logo.png" alt="Accessible Housing Austin! AHA!">
+		</a>
         </header>        
     </div>
     <div class="col-md-8" style="margin-top:5em;">
--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<address class="vcard">
-		<p class="org">Accessible Housing Austin</p>
+		<p class="org">Accessible Housing Austin!</p>
 		<p class="adr">
 			<span class="street-address">1640-A E. 2nd Street</span>,
 			<span class="locality">Austin</span>,
@@ -8,7 +8,7 @@
 			<span class="postal-code">78702</span>
 		</p>
 	</address>
-	<a href="http://knowbility.org/"><img src="../images/knowbility.png" alt="Knowbility" style="background-color: #000; color: #fff; border: solid #000 5px;"></a>
+	<a href="http://knowbility.org/"><img src="../images/knowbility.png" alt="Knowbility" style="background-color: #000; color: #fff; border: solid #000 5px; width: 150px; height: 40px;"></a>
 	<p style="display: block;">Last modified: December 7, 2013</p>
 </footer>
 
