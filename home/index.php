9c0d46945b7e43acb8652e6702d91883a15b756c
George Luc <george@givepulse.com>
2013-12-07 00:26:33 -0600

    Fixes #50

--- home/index.php
+++ home/index.php
@@ -32,7 +32,7 @@
             <form style="margin-top:1em;" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="donate">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="hosted_button_id" value="99VH99YEQSKE8">
-                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="Donate via PayPal">
+                <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" name="submit" alt="Donate via PayPal">
                 <img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
             </form>
             
